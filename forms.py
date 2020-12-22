3ccd8e484eb81800c71a1c82e8a7ce38c769da97
Konrad Gawron <konrad.gawron@poczta.fm>
2020-12-22 22:06:05 +0100

    Add files with contents

---
+++ forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, BooleanField
+from wtforms.validators import DataRequired
+
+class TodoForm(FlaskForm):
+    title = StringField('title', validators=[DataRequired()])
+    details = TextAreaField('details', validators=[DataRequired()])
+    finish = BooleanField('taks is done', validators=[DataRequired()])
+
+
+
+
